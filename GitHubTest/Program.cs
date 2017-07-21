2b651aa901db846d9e359d9979be2c298d0ad991
Mesut OZTURK <mesutoztrk@msn.com>
2017-07-21 10:40:29 +0300

    Ekleme yapıldı

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub");
+            Console.WriteLine("fmakmfklasmlkdmsalkd");
         }
     }
 }
